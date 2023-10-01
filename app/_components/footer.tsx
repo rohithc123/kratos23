0dddda50fcafd28fbb3e4d606a3a51021d262cb8
Nithish <nithssh@protonmail.com>
2023-10-01 18:44:29 +0530

    Add footer to layout

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,9 +1,38 @@
+import Image from 'next/image';
+import dots from '@/public/dotted-pattern.png';
+import logo from '@/public/logo.png';
+import instagram from '@/public/instagram.png';
+import Link from 'next/link';
 
-
-// TODO
 export default function Footer() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <footer className="flex flex-col items-center w-screen relative p-6 pt-12 gap-6">
+      {/* bg */}
+      <Image src={dots} alt="" className="position absolute inset-0 z-[-1]" />
+
+      {/* Logo */}
+      <Image src={logo} alt="Kratos 23 logo" width={64} height={64} />
+
+      {/* Instagram link */}
+      <Link href='https://www.instacolorgram.com/_kratos23/' className="flex">
+        <Image src={instagram} alt="Instagram logo" width={24} height={24} />
+        &ensp;_kratos23
+      </Link>
+
+      {/* HR */}
+      <div className='h-[1px] w-full bg-void-500'/>
+
+      {/* Links */}
+      <div className='flex flex-col items-center gap-4 text-void-300 font-normal'>
+        <Link href='/terms'>Terms of Service</Link>
+        <Link href='/privacy'>Privacy Policy</Link>
+        <Link href='/conduct'>Code of Conduct</Link>
+        <Link href='https://github.com/nithssh/kratos23'>GitHub repo</Link>
+      </div>
+
+      {/* Copyright */}
+
+      <p className='text-void-500'>© 2023 Kratos</p>
+    </footer>
+  );
 }
