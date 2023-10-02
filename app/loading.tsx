2275e31faf80613bc1466a57f98f9fcac8ff38ab
Nithish <nithssh@protonmail.com>
2023-10-02 18:03:42 +0530

    Add placeholder root loading screen

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Loading() {
+    return (
+        <div className="h-screen w-screen bg-cherry text-black grid place-content-center">
+            TODO Loading...
+        </div>
+    )
+}
