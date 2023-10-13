4e125e2fb604d7c855832e8dd573bae68c0da86a
Nithish <nithssh@protonmail.com>
2023-10-13 18:08:58 +0530

    Improve the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import art_ribbon from '@/public/art_ribbon.jpg'
 import expand_more from '@/public/expand_more.svg'
 import logo from '@/public/logo.png'
+import logo_bw from '@/public/logo-bw.png'
 import nontech_ticket from '@/public/non-tech.png'
 import tech_ticket from '@/public/tech.png'
 import { Josefin_Sans, Libre_Baskerville } from 'next/font/google'
@@ -12,56 +13,63 @@ const josefinSans = Josefin_Sans({ weight: '400', subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      {/* Spacer */}
-      <div className="p-10" />
-      {/* Hero Image */}
-      <div
-        style={{ width: `min(calc(100vw - 64px), 400px)` }}
-        className="relative aspect-square m-8 grid place-content-center rounded-[32px] bg-gradient-to-br from-cherry/10 to-vinyl/10"
-      >
-        {/* Title */}
-        <div style={baskerville.style} className="z-10 text-5xl text-center">
-          KRATOS
+    <main className="flex flex-col items-center">
+      <header className="pt-20 flex flex-col items-center h-[100svh] justify-around">
+        {/* Hero Image */}
+        <div
+          className="relative aspect-square min-w-[85vw] m-8 grid place-content-center rounded-[32px] bg-gradient-to-br from-cherry/10 to-vinyl/10"
+        >
+          {/* Title */}
+          <h1 style={baskerville.style} className="z-10 text-5xl text-center">
+            KRATOS
+          </h1>
+          <h1 style={josefinSans.style} className="z-10 text-6xl text-center">
+            23
+          </h1>
+
+          {/* Logo */}
+          {/* <Image
+            className="opacity-[.25] z-[2] object-contain absolute top-2/4 -translate-y-1/2 left-2/4 -translate-x-1/2"
+            src={logo_bw}
+            alt="Vector art of a lion"
+          /> */}
+
+          {/* Blurred Pic BG */}
+          <Image
+            className="blur-[100px] z-[1] object-contain absolute top-2/4 -translate-y-1/2 left-2/4 -translate-x-1/2"
+            src={logo}
+            alt=""
+          />
         </div>
-        <div style={josefinSans.style} className="z-10 text-6xl text-center">
-          23
+        {/* Hero Text */}
+        <div className="text-5xl px-4 tracking-tighter text-center font-extrabold text-transparent">
+          <span className="bg-clip-text bg-gradient-to-r from-cherry to-vinyl">
+            Code.&thinsp;
+          </span>
+          <span className="bg-clip-text bg-gradient-to-r from-white to-white/50">
+            Compete.&thinsp;
+          </span>
+          <span className="bg-clip-text bg-gradient-to-r from-white to-white/50">
+            Conquer.&thinsp;
+          </span>
         </div>
 
-        {/* Logo */}
-        {/* <Image className='opacity-[.25] z-[2] object-contain absolute top-2/4 -translate-y-1/2 left-2/4 -translate-x-1/2' src={logo_bw} alt='logo'/> */}
-
-        {/* Blurred Pic BG */}
+        <div className="text-2xl text-center mt-8">
+          <div className="">November 9</div>
+          <div className="text-void-300">@ Easwari Engineering College</div>
+        </div>
         <Image
-          className="blur-[100px] z-[1] object-contain absolute top-2/4 -translate-y-1/2 left-2/4 -translate-x-1/2"
-          src={logo}
-          alt="logo"
+          src={expand_more}
+          alt=""
+          width={58}
+          height={58}
+          className="m-12 animate animate-bounce"
         />
+      </header>
+
+      <div className="mx-4 rounded-lg overflow-hidden">
+        <video className="" src="/promo.mp4" autoPlay muted loop />
       </div>
-      {/* Hero Text */}
-      <div className="text-5xl px-4 tracking-tighter text-center font-extrabold text-transparent">
-        <span className="bg-clip-text bg-gradient-to-r from-cherry to-vinyl">
-          Code.&thinsp;
-        </span>
-        <span className="bg-clip-text bg-gradient-to-r from-white to-white/50">
-          Compete.&thinsp;
-        </span>
-        <span className="bg-clip-text bg-gradient-to-r from-white to-white/50">
-          Conquer.&thinsp;
-        </span>
-      </div>
-      <div className="text-2xl text-center mt-8">
-        <div className="">November 9</div>
-        <div className="text-void-300">@ Easwari Engineering College</div>
-      </div>
-      <Image
-        src={expand_more}
-        alt=""
-        width={58}
-        height={58}
-        className="m-8 animate animate-bounce"
-      />
-      {/* TODO add video over here */}
 
       {/* Art Ribbon Divider */}
       <Image
