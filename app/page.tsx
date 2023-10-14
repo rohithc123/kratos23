80e5f28cbed575f59113133badeb5069f653d0cc
Rohithc123 <rohithc.eec.cseb@gmail.com>
2023-10-14 22:22:27 +0530

    styling fixes for homepage kratos image

--- app/page.tsx
+++ app/page.tsx
@@ -24,9 +24,9 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center md:max-w-[768px]">
-      <header className="pt-20 flex flex-col items-center h-[100svh] md:h-screen justify-around">
+      <header className="pt-20 flex flex-col items-center min-h-[100svh] md:h-screen justify-around">
         {/* Hero Image */}
-        <div className="relative aspect-square w-[85vw] max-w-[330px] lg:max-w-[550px] m-8 grid place-content-center rounded-[32px] bg-gradient-to-br from-cherry/10 to-vinyl/10">
+        <div className="relative aspect-square w-[85vw] max-w-[330px] lg:max-w-[400px] lg:m-8 grid place-content-center rounded-[32px] bg-gradient-to-br from-cherry/10 to-vinyl/10">
           {/* Title */}
           <h1 style={baskerville.style} className="z-10 text-5xl text-center">
             KRATOS
