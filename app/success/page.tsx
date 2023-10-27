1402f9d9684cdf2612672b5645767e9bd7a60cdd
Nithish <nithssh@protonmail.com>
2023-10-27 21:54:07 +0530

    Clear bag after successful registration

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,11 +1,41 @@
-import { Poly } from 'next/font/google'
+'use client'
+
 import arrow_left from '@/public/arrow_left.svg'
+import { Poly } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useEffect } from 'react'
+import {
+  Selected,
+  getCookie,
+  removeSelectedEvent,
+  removeTeamDetails,
+} from '../cookies'
 
 const poly = Poly({ weight: '400', subsets: ['latin'] })
 
 export default function Success() {
+  useEffect(() => {
+    // clear the bag after checkout. This page is only reached when
+    // user has successfully registered, so clear the bag on this page's load.
+
+    // Note: not clearing the personal details cookie, since user may register for
+    // other events after this anyways
+
+    // TODO Maybe consider making another cookie, that stores the added events
+    // to a diff cookie, which is then used by the event details page to mark the
+    // button as already registered
+    if (typeof window !== undefined) {
+      const addedEvents = getCookie<Selected>('selected')!.events
+      // remove all the team details
+      for (let ev of addedEvents) {
+        // this function is safe to call on solo events too -- it does nothing.
+        removeTeamDetails(ev)
+        removeSelectedEvent(ev)
+      }
+    }
+  })
+
   return (
     <main className="min-h-screen flex flex-col items-center justify-center text-center gap-6 px-6">
       <h1 style={poly.style} className="text-5xl">
@@ -15,8 +45,11 @@ export default function Success() {
         You will receive email confirmation later from the organizers.
         We&apos;ll see you on campus on 3rd November!
       </p>
-      <Link href='/' className="bg-gradient-to-br from-cherry to-vinyl w-full rounded-full text-void-950 p-3 flex justify-center font-semibold">
-        <Image src={arrow_left} width={24} height={24} alt='' />
+      <Link
+        href="/"
+        className="bg-gradient-to-br from-cherry to-vinyl w-full rounded-full text-void-950 p-3 flex justify-center font-semibold"
+      >
+        <Image src={arrow_left} width={24} height={24} alt="" />
         Back to home page
       </Link>
     </main>
